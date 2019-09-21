974f1b9c830c0ebbd5d4ce722ef93c9df070cc15
Andres Ruiz Graciano <aandres.r98@gmail.com>
2019-09-21 13:59:46 -0500

    Se anade metodo para llenar automaticamente las preguntas cuando el tiempo se agote, todas se pierden

--- src/app/components/exam/question/question.component.ts
+++ src/app/components/exam/question/question.component.ts
@@ -11,11 +11,12 @@ export class QuestionComponent implements OnInit {
   @Input() tests: any;
   @Input() user: any;
   @Input() exam: any;
+
   paragraph = ``;
   btnMessage = "Enviar y Continuar";
   currentQuestion = 1;
   totalQuestion: number;
-  studenAnswers = [];
+  studentAnswers = [];
   answer = 0;
   btnDisabled = "";
   minutes: number;
@@ -27,8 +28,8 @@ export class QuestionComponent implements OnInit {
   ngOnInit() {
     this.totalQuestion = this.tests.length;
     this.paragraph = this.tests[this.currentQuestion - 1].problem.question;
-    this.minutes = this.exam.duration - 1;
-    this.seconds = 59;
+    this.minutes = this.exam.duration;
+    this.seconds = 0;
     this.timer = setInterval(() => this.tick(), 1000);
   }
 
@@ -36,33 +37,29 @@ export class QuestionComponent implements OnInit {
     let studentAnswer = {
       id_student: this.user.idStudent,
       id_group: this.user.idGroup,
-      id_test: 0,
+      id_test: this.tests[this.currentQuestion - 1].test.id,
       failed: true,
       date: new Date().toISOString().split("T")[0]
     }
-    if (this.currentQuestion == this.totalQuestion) {
-      studentAnswer.id_test = this.tests[this.currentQuestion - 1].test.id;
-      if (this.answer == this.tests[this.currentQuestion - 1].problem.answer) {
-        studentAnswer.failed = false
-      } else {
-        studentAnswer.failed = true;
-      }
-      this.studenAnswers.push(studentAnswer);
-      this.btnDisabled = "true";
 
-      this.exaService.insertStudentAnswers(this.studenAnswers).subscribe(data => {
-        console.log("Mostrar las pinches estadisticas");
-      }, error => {
-        console.log(error);
-      });
-    } else {
-      studentAnswer.id_test = this.tests[this.currentQuestion - 1].test.id;
+    if (this.currentQuestion == this.totalQuestion) {
       if (this.answer == this.tests[this.currentQuestion - 1].problem.answer) {
         studentAnswer.failed = false
       } else {
         studentAnswer.failed = true;
       }
-      this.studenAnswers.push(studentAnswer);
+      this.studentAnswers.push(studentAnswer);
+      this.btnDisabled = "true";
+      this.saveStudentAnswers();
+      clearInterval(this.timer);
+
+    } else {
+      if (this.answer == this.tests[this.currentQuestion - 1].problem.answer) {
+        studentAnswer.failed = false
+      } else {
+        studentAnswer.failed = true;
+      }
+      this.studentAnswers.push(studentAnswer);
       this.currentQuestion += 1;
 
       if (this.currentQuestion == this.totalQuestion) {
@@ -75,12 +72,42 @@ export class QuestionComponent implements OnInit {
 
   tick() {
     if (--this.seconds < 0) {
-      clearInterval(this.timer);
-      this.seconds = 59
+      this.seconds = 59;
       if (--this.minutes < 0) {
+        this.fillStudentAnswers();
+        clearInterval(this.timer);
         this.minutes = 0;
         this.seconds = 0;
       }
     }
+  }
+
+  saveStudentAnswers() {
+    this.exaService.insertStudentAnswers(this.studentAnswers).subscribe(data => {
+      if (data.status == 200) {
+        //Mostrar las estadisticas
+        console.log("Datos guardados con éxito");
+      } else {
+        console.log('No es el status code');
+      }
+    }, err => {
+      console.log("Problemas en la conexión a la BD", err)
+    });
+  }
+
+  fillStudentAnswers() {
+    this.btnDisabled = "true";
+    for (let i = this.currentQuestion; i <= this.totalQuestion; i++) {
+      let studentAnswer = {
+        id_student: this.user.idStudent,
+        id_group: this.user.idGroup,
+        id_test: this.tests[i - 1].test.id,
+        failed: true,
+        date: new Date().toISOString().split("T")[0]
+      }
+
+      this.studentAnswers.push(studentAnswer);
+    }
+    this.saveStudentAnswers();
   }
 }
